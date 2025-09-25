868c3146a05f146fe437558d2bf06c4508b12d59
Potsawee <potsaweee.p@gmail.com>
2025-09-25 13:21:04 +0700

    remove unused function in main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ async def get_pokemon():
             return RedirectResponse(url="/", status_code=302)
         df = pd.DataFrame(
             pokemon_list, 
-            # columns=["number", "name", "image", "type","stats"]
             columns=["Number", "Name", "Type", "All", "HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed", "image"]
         )
         return StreamingResponse(
@@ -29,36 +28,10 @@ async def get_pokemon():
     except Exception as e:
         return RedirectResponse(url="/", status_code=302)
 
-def get_filtered_data(data: list, query: str):
-    results = []
-    print(query)
-    try:
-        pattern = re.compile(query, re.IGNORECASE)        
-        for row in data:
-            if 'Name' in row and pattern.search(row['Name']):
-                results.append(row)
-        return results
-    except re.error as e:
-        print(f"Invalid regex query: {e}")
-        return []
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return []
-
-
-@app.get("/get-query", response_class=JSONResponse)
-async def query(query: str = Query(None, description="The name or regex pattern to search for")):
-    results = get_filtered_data(query)
-    return results
-
-
 @app.get("/list-items", response_class=HTMLResponse)
 async def read_item(request: Request, query: str = Query(None)):
     try:
         pokemon_list = crawling(query)
-        # if not pokemon_list:
-        #     return RedirectResponse(url="/", status_code=302)
-
         return templates.TemplateResponse(
             "list_crawl.html", {"request": request, "data": pokemon_list, "query_text": query}
         )
